818b42256a33ae3b3fc2bc69d5120ff4c18ff390
M-Ridha <82166718+M-Ridha@users.noreply.github.com>
2024-09-10 15:19:44 +0100

    api

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 function App() {
+  const [userList,setUserList]=useState([])
+  
+  const getUsers = async () => {
+    try {
+
+      const users = await axios.get("https://jsonplaceholder.typicode.com/users")
+      setUserList(users.data)
+
+    } catch (error) {
+      console.log('error', error)
+    }
+  }
+
+  useEffect(()=>{
+    getUsers()
+  },[])
+
+  console.log('userList', userList)
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {
+          userList?.length ? 
+          userList.map((el,i)=>(
+            <div>
+              <hr></hr>
+              <h1> {el.name} </h1>
+              <h1> {el.username} </h1>
+              <h1> {el.phone} </h1>
+              <h1> {el.address.city} </h1>
+              <hr></hr>
+            </div>
+          )) :
+          <h1> Loading... </h1>
+        }
       </header>
     </div>
   );
